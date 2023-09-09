f5d17bc70f9ade4dba6fb89d1ea2f91da95c9b56
Sameer9619 <ansarisameer9619@gmail.com>
2023-09-09 19:44:18 +0530

    first commit

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,36 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    // const phrase = "Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.";
+     const phrase="A full-stack digital marketing company that uses a consultative approach combining creativity and technology to solve marketing problems."
+
+     
+   
+    const description = useRef(null);
+
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map( (word, index) => {
+                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                    })
+                }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>
+There is no one-size-fits-all solution in the digital world, hence we focus on developing customised marketing strategies.</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Projects/components/project/index.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import styles from './style.module.scss';
+
+export default function index({index, title, manageModal}) {
+
+    return (
+        <div onMouseEnter={(e) => {manageModal(true, index, e.clientX, e.clientY)}} onMouseLeave={(e) => {manageModal(false, index, e.clientX, e.clientY)}} className={styles.project}>
+            <h2>{title}</h2>
+            <p>Digital transformation of a Celebrity led Brand to India’s largest Home-grown Athleisure Brand in the span of 5 years.</p>
+        </div>
+    )
+}
